6499eb2f3d9e5a2ee536bc626e76cda56833988d
gfs980 <48533811+gfs980@users.noreply.github.com>
2020-12-27 18:00:30 +0000

    testing other case scenarios for time stamps

--- tests/timeStamps.test.ts
+++ tests/timeStamps.test.ts
@@ -6,3 +6,19 @@ const [FordFiesta, MercSCoupe, MercGWagen] = vehicles;
 it('should calculate the age of the fordFiesta as 7 years even that it is 7 and 6 months old', () => {
     expect(fromDateYearsCalculator(FordFiesta.first_registration_date)).toEqual(7);
 })
+
+it('should calculate the age of the MercSCoupe as 3 years even that it is 3 and 3 months old', () => {
+  expect(fromDateYearsCalculator(MercSCoupe.first_registration_date)).toEqual(3);
+})
+
+it('should calculate the age of the Mercedes G-Wagen as 2 years even that it is 2 and 3 months old', () => {
+  expect(fromDateYearsCalculator(MercGWagen.first_registration_date)).toEqual(2);
+})
+
+it('should calculate the age of the test date as 2 years even that it is 2 and 11 months old', () => {
+  expect(fromDateYearsCalculator(new Date('January 2018'))).toEqual(2);
+})
+
+it('should calculate the age of the test2 date as 0 years even that it is 1 month old', () => {
+  expect(fromDateYearsCalculator(new Date('11 November 2020'))).toEqual(0);
+})
