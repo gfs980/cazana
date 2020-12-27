ab93ec52a5224cc64fd6e45074324e5317066787
gfs980 <48533811+gfs980@users.noreply.github.com>
2020-12-27 18:12:19 +0000

    testing the other cars for annual mileage

--- tests/timeLineEvents.test.ts
+++ tests/timeLineEvents.test.ts
@@ -8,3 +8,13 @@ it('Find annual mileage for Ford Fiesta', () => {
   expect(TimeLineEvents.findAnnualMileageEstimate(FordFiesta.mots[0].mileage, FordFiesta.first_registration_date))
       .toBe(16556.14)
 })
+
+it('Find annual mileage for MercSCoupe', () => {
+  expect(TimeLineEvents.findAnnualMileageEstimate(MercSCoupe.sale_adverts[0].mileage, MercSCoupe.first_registration_date))
+      .toBe(5494.67)
+})
+
+it('Find annual mileage for MercGWagen', () => {
+  expect(TimeLineEvents.findAnnualMileageEstimate(MercGWagen.sale_adverts[0].mileage, MercGWagen.first_registration_date))
+      .toBe(8401)
+})
